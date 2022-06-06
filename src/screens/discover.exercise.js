72cfd793faedd6cfd77fe5990d77c747c665a110
Ethiobek <arkishasoftware@gmail.com>
2022-06-06 16:51:21 +0300

    Note update functionality

--- src/screens/discover.exercise.js
+++ src/screens/discover.exercise.js
@@ -5,7 +5,8 @@ import * as React from 'react'
 import Tooltip from '@reach/tooltip'
 import {FaSearch, FaTimes} from 'react-icons/fa'
 // 🐨 you'll need useQuery from 'react-query'
-import {useAsync} from 'utils/hooks'
+import {useQuery} from 'react-query'
+// import {useAsync} from 'utils/hooks'
 import {client} from 'utils/api-client'
 import * as colors from 'styles/colors'
 import {BookRow} from 'components/book-row'
@@ -33,20 +34,33 @@ function DiscoverBooksScreen({user}) {
   // the queryKey should be ['bookSearch', {query}]
   // the queryFn should be the same thing we have in the run function below
   // you'll get back the same stuff you get from useAsync, (except the run function)
-  const {data, error, run, isLoading, isError, isSuccess} = useAsync()
+  // const {data, error, run, isLoading, isError, isSuccess} = useAsync()
 
-  const books = data ?? loadingBooks
-
+  //to ignore capitalization SEO both SA and sa should be consider the same input Lion and lion
   React.useEffect(() => {
-    if (!queried) {
-      return
-    }
-    run(
+    setQuery(query.toLowerCase())
+  }, [query])
+
+  const {data, error, isLoading, isError, isSuccess} = useQuery({
+    queryKey: ['searchBook', {query}],
+    queryFn: () =>
       client(`books?query=${encodeURIComponent(query)}`, {
         token: user.token,
       }).then(data => data.books),
-    )
-  }, [query, queried, run, user.token])
+  })
+
+  const books = data ?? loadingBooks
+
+  // React.useEffect(() => {
+  //   if (!queried) {
+  //     return
+  //   }
+  //   run(
+  //     client(`books?query=${encodeURIComponent(query)}`, {
+  //       token: user.token,
+  //     }).then(data => data.books),
+  //   )
+  // }, [query, queried, run, user.token])
 
   function handleSearchSubmit(event) {
     event.preventDefault()
